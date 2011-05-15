9a0d4140878c91f1ab0d308180edc9a50f1fde01
dan_plan <services@plancake.com>
2011-05-15 01:34:50 +0100

    change of license

--- PlancakeEmailParser.php
+++ PlancakeEmailParser.php
@@ -7,15 +7,15 @@
 *                                                                                    *
 * Copyright 2009-2010-2011 by:     Danyuki Software Limited                          *
 * Support, News, Updates at:  http://www.plancake.com                                *
-* Licensed under the AGPL version 3 license.                                         *                                                       *
+* Licensed under the LGPL version 3 license.                                         *                                                       *
 * Danyuki Software Limited is registered in England and Wales (Company No. 07554549) *
 **************************************************************************************
 * Plancake is distributed in the hope that it will be useful,                        *
 * but WITHOUT ANY WARRANTY; without even the implied warranty of                     *
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      *
-* GNU Affero General Public License for more details.                                *
+* GNU Lesser General Public License v3.0 for more details.                           *
 *                                                                                    *
-* You should have received a copy of the GNU Affero General Public License           *
+* You should have received a copy of the GNU Lesser General Public License           *
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.              *
 *                                                                                    *
 **************************************************************************************/
